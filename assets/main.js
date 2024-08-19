49c6b28cb12a08fb62ccde0733780c42fc0c2651
kuni <ahmad.baikuni17@pwk.undip.ac.id>
2024-08-19 15:43:37 +0700

    add layer control

---
+++ assets/main.js
@@ -0,0 +1,87 @@
+// TO MAKE THE MAP APPEAR YOU MUST
+// ADD YOUR ACCESS TOKEN FROM
+// https://account.mapbox.com
+mapboxgl.accessToken = 'pk.eyJ1IjoiYmFpa3VuaXAxNCIsImEiOiJjbDBycXdmbHEwNjY0M2lrN2lubnd1aW43In0.zsbYN2VrQ1Dit4kj4uhzWw';
+const map = new mapboxgl.Map({
+    container: 'map', // container ID
+    center: [10.2994,54.073831], // starting position [lng, lat]. Note that lat must be set between -90 and 90
+    zoom: 12, // starting zoom
+    minZoom:7,
+    maxZoom:16,
+    style: 'mapbox://styles/mapbox/satellite-streets-v12'
+});
+map.on('load', () => {
+    map.addSource('datapoints', {
+        type: 'vector',
+        // Use any Mapbox-hosted tileset using its tileset id.
+        // Learn more about where to find a tileset id:
+        // https://docs.mapbox.com/help/glossary/tileset-id/
+        url: 'mapbox://baikunip14.51ogwkf4'
+    });
+    map.addLayer({
+        'id': 'point',
+        'source': 'datapoints',
+        'source-layer': 'Stromerzeuger_1_bis_36666-48glzr',
+        'type': 'circle',
+        'paint': {
+            'circle-radius': 4,
+            'circle-color': '#FFFC62'
+        }
+    });
+    map.addLayer({
+        'id': 'point-heat',
+        'type': 'heatmap',
+        'source': 'datapoints',
+        'source-layer': 'Stromerzeuger_1_bis_36666-48glzr',
+        'maxzoom': 9,
+        'paint':{
+            'heatmap-intensity': [
+                'interpolate',
+                ['linear'],
+                ['zoom'],
+                0,
+                1,
+                9,
+                3
+            ],
+            'heatmap-color': [
+                'interpolate',
+                ['linear'],
+                ['heatmap-density'],
+                0,
+                'rgba(255,252,28,0)',
+                0.2,
+                'rgb(255,252,28)',
+                0.4,
+                'rgb(209,229,240)',
+                0.6,
+                'rgb(253,219,199)',
+                0.8,
+                'rgb(239,138,98)',
+                1,
+                'rgb(178,24,43)'
+            ],
+            // Adjust the heatmap radius by zoom level
+            'heatmap-radius': [
+                'interpolate',
+                ['linear'],
+                ['zoom'],
+                0,
+                2,
+                9,
+                20
+            ],
+            // Transition from heatmap to circle layer by zoom level
+            'heatmap-opacity': [
+                'interpolate',
+                ['linear'],
+                ['zoom'],
+                7,
+                1,
+                9,
+                0
+            ]
+        }
+    });
+})
+// Layer Control Functionalities
